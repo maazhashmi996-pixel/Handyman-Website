a90cc5c2420bd5ce472ae19483baf76566fcf397
Maaz Hashmi <maazhashmi996-pixel>
2026-03-08 19:18:51 +0500

    Updated Theme of Service Section

--- components/FinalSection.tsx
+++ components/FinalSection.tsx
@@ -13,8 +13,7 @@ import {
 
 export default function FinalSectionCommand() {
     return (
-        /* BACKGROUND: Ab ye black nahi, ek vibrant Deep Electric Violet base hai */
-        /* Vibrant Radial Gradient Overlay jo color ko 'pop' karega */
+
         <section className="relative overflow-hidden px-6 py-32 bg-[#0d001a] selection:bg-cyan-500 selection:text-white">
 
             {/* Background Mesh Gradient - Vibrant Blobs */}
