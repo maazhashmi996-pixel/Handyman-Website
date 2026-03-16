f88dd1d1eda964c3dacceb91d4ac2c168c915c27
Maaz Hashmi <maazhashmi996-pixel>
2026-03-16 15:36:06 +0500

    Updated Handyman Sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 
 import AboutApexSection from "@/components/AboutApexSection";
 import FinalSectionCommand from "@/components/FinalSection";
+import UpdatedHandymanHero from "@/components/hero";
 import HeroSection from "@/components/hero";
 import UltraWorkflow from "@/components/ProcessSection";
 import ServiceGrid from "@/components/ServiceGrid";
@@ -9,7 +10,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <HeroSection />
+      <UpdatedHandymanHero />
       <AboutApexSection />
       <ServiceGrid />
       <UltraWorkflow />
